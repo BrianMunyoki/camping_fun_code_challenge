30c9bcd13ef1ec69f799bcb8c13bc81af37dcf43
brianmunyoki <brianmunyoki168@gmail.com>
2025-11-10 22:34:25 +0300

    seeding

--- server/models.py
+++ server/models.py
@@ -1,39 +1,47 @@
 from flask_sqlalchemy import SQLAlchemy
 from app import db
 
+
 class Camper(db.Model):
-    __tablename__='campers'
+    __tablename__ = 'campers'
 
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.String, nullable=False)
-    age=db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    age = db.Column(db.Integer)
 
-    #relationships
-    signups=db.relationships('Signup',back_populates='Camper',cascade='all, delete-orphan')
+    # Relationship
+    signups = db.relationship('Signup', back_populates='camper', cascade='all, delete-orphan')
 
     def __repr__(self):
-        return f'<camper{self.name}'
+        return f'<Camper {self.name}>'
+
 
 class Activity(db.Model):
-     __tablename__='activities'
-    id=db.Column(db.Integer, primary_key=True)
-    name=db.Column(db.string)
-    difficulty=db.Column(db.string)
-    #relationships
-    signups=db.relationships('signup', back_populates='Activity',cascade='all, delete orphan')
+    __tablename__ = 'activities'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    difficulty = db.Column(db.String)
+
+    # Relationship
+    signups = db.relationship('Signup', back_populates='activity', cascade='all, delete-orphan')
 
     def __repr__(self):
         return f'<Activity {self.name}>'
-class Signup(db.Model):
-     __tablename__='signups'
-    id=db.Column(db.Integer,primary_key=True)
-    time=db.Column(db.Integer)
 
-    camper_id=db.Column(db.Integer, db.ForeignKey('Campers.id'))
-    activity_id=db.Column(db.Iteger, db.ForeignKey('activities.id'))
-    #relationships
-    camper=db.relationship('Camper',back_populates='Signups')
-    activity=db.relationship('Activity', back_populates='signups')
+
+class Signup(db.Model):
+    __tablename__ = 'signups'
+
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Integer)
+
+    camper_id = db.Column(db.Integer, db.ForeignKey('campers.id'))
+    activity_id = db.Column(db.Integer, db.ForeignKey('activities.id'))
+
+    # Relationship
+    camper = db.relationship('Camper', back_populates='signups')
+    activity = db.relationship('Activity', back_populates='signups')
 
     def __repr__(self):
-        return f'<Signup Camper={self.camper_id} Activity={self.activity_id}'
+        return f'<Signup Camper={self.camper_id} Activity={self.activity_id}>'
---
+++ server/seed.py
@@ -0,0 +1,19 @@
+from app import app, db
+from models import Camper, Activity, Signup
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    camper1=Camper(name="Brian",age=25)
+    camper2=Camper(name="Jane",age=22)
+
+    activity1=Activity(name="Hiking",difficulty="Medium")
+    activity2=Activity(name="Swimming",difficulty="Easy")
+
+    signup1=Signup(time=9, camper=camper1, activity=activity1)
+    signup2=Signup(time=10, camper=camper1, activity=activity2)
+    signup3=Signup(time=9, camper=camper2, activity=activity2)
+
+    db.session.add_all([camper1,camper2,activity1,activity2,signup1,signup2,signup3])
+    db.session.commit()
