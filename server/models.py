125e9fff6470e7eae471107a6c19e57b7c859fb1
brianmunyoki <brianmunyoki168@gmail.com>
2025-11-10 05:43:19 +0300

    signups relationship means one camper can have many sign ups  && cascade=All, delete-orphan ensures that if a camper is deleted, their sign ups are deleted too

--- server/models.py
+++ server/models.py
@@ -8,4 +8,10 @@ from app import db
     name=db.Column(db.String, nullable=False)
     age=db.Column(db.Integer)
 
-  
+    #relationships
+    signups=db.relationships('Signup',back_populates='Camper',cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<camper{self.name}'
+
+    
